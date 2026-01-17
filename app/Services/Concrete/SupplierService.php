2fbc3b8e95c592d3445c952fff03e8522ce8420c
Aadi Malik <adnanjunaid76279@gmail.com>
2026-01-17 15:51:19 +0500

    supplier balance color change

--- app/Services/Concrete/SupplierService.php
+++ app/Services/Concrete/SupplierService.php
@@ -57,25 +57,25 @@ class SupplierService
             ->addColumn('balance', function ($item) {
                 if($item->account_id!=null){
                     $balance = $this->journal_entry_service->getSupplierBalanceByAccountId($item->id,$item->account_id,0);
-                    return ($balance>0)?"<span class='btn-success pl-1'> <i class='fa fa-arrow-up'></i>".$balance."</span> ":(($balance<0)?"<span class='btn-danger pl-1'> <i class='fa fa-arrow-down'></i>".$balance."</span>":"<span class='btn-primary pl-1'> <i class='fa fa-arrows'></i>".(($balance==null)?0:$balance)."</span>");
+                    return ($balance>0)?"<span class='btn-danger pl-1'> <i class='fa fa-arrow-up'></i>".$balance."</span> ":(($balance<0)?"<span class='btn-success pl-1'> <i class='fa fa-arrow-down'></i>".$balance."</span>":"<span class='btn-primary pl-1'> <i class='fa fa-arrows'></i>".(($balance==null)?0:$balance)."</span>");
                 }else{
-                    return "<span class='btn-danger pl-1'> <i class='fa fa-arrows'></i>0</span>";
+                    return "<span class='btn-success pl-1'> <i class='fa fa-arrows'></i>0</span>";
                 }
             })
             ->addColumn('balance_au', function ($item) {
                 if($item->account_id!=null){
                     $balance = $this->journal_entry_service->getSupplierBalanceByAccountId($item->id,$item->account_id,1);
-                    return ($balance>0)?"<span class='btn-success pl-1'> <i class='fa fa-arrow-up'></i>".$balance."</span> ":(($balance<0)?"<span class='btn-danger pl-1'> <i class='fa fa-arrow-down'></i>".$balance."</span>":"<span class='btn-primary pl-1'> <i class='fa fa-arrows'></i>".(($balance==null)?0:$balance)."</span>");
+                    return ($balance>0)?"<span class='btn-danger pl-1'> <i class='fa fa-arrow-up'></i>".$balance."</span> ":(($balance<0)?"<span class='btn-success pl-1'> <i class='fa fa-arrow-down'></i>".$balance."</span>":"<span class='btn-primary pl-1'> <i class='fa fa-arrows'></i>".(($balance==null)?0:$balance)."</span>");
                 }else{
-                    return "<span class='btn-danger pl-1'> <i class='fa fa-arrows'></i>0</span>";
+                    return "<span class='btn-success pl-1'> <i class='fa fa-arrows'></i>0</span>";
                 }
             })
             ->addColumn('balance_dollar', function ($item) {
                 if($item->account_id!=null){
                     $balance = $this->journal_entry_service->getSupplierBalanceByAccountId($item->id,$item->account_id,2);
-                    return ($balance>0)?"<span class='btn-success pl-1'> <i class='fa fa-arrow-up'></i>".$balance."</span> ":(($balance<0)?"<span class='btn-danger pl-1'> <i class='fa fa-arrow-down'></i>".$balance."</span>":"<span class='btn-primary pl-1'> <i class='fa fa-arrows'></i>".(($balance==null)?0:$balance)."</span>");
+                    return ($balance>0)?"<span class='btn-danger pl-1'> <i class='fa fa-arrow-up'></i>".$balance."</span> ":(($balance<0)?"<span class='btn-success pl-1'> <i class='fa fa-arrow-down'></i>".$balance."</span>":"<span class='btn-primary pl-1'> <i class='fa fa-arrows'></i>".(($balance==null)?0:$balance)."</span>");
                 }else{
-                    return "<span class='btn-danger pl-1'> <i class='fa fa-arrows'></i>0</span>";
+                    return "<span class='btn-success pl-1'> <i class='fa fa-arrows'></i>0</span>";
                 }
             })
             ->addColumn('status', function ($item) {
